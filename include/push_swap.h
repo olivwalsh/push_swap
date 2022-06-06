7d324be1174b6df1abd8570ed884a5b160f39d43
Olivia WALSH <owalsh@e2r7p8.clusters.42paris.fr>
2022-06-06 15:43:21 +0200

    Add project structure

---
+++ include/push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/06 15:34:09 by owalsh            #+#    #+#             */
+/*   Updated: 2022/06/06 15:42:33 by owalsh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include "libft/libft.h"
+
+typedef struct element
+{
+	number	*previous;
+	number	*next;
+	int		num;
+	int		index;
+}			number;
+
+#endif
